ca84deb27bd41fde0ad79052bfeaeb4b4f298ca6
Kasak saxena <158446050+Kasaksaxena@users.noreply.github.com>
2025-08-18 19:44:27 +0530

    33515

--- home/views.py
+++ home/views.py
@@ -5,6 +5,7 @@ from .models import RestaurantInfo
 def homepage(request):
     restaurant=RestaurantInfo.objects.first()
     context={
-        'restaurant_name':restaurant.name if restaurant else "Our Restaurant"
+        'restaurant_name':restaurant.name if restaurant else "Our Restaurant",
+        'phone_number':settings.RESTAURANT_PHONE_NUMBER
     }
     return render(request,'home/index.html',context)
