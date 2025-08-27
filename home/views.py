c656e0d510da0579eef1231de3eb987765872e5e
Kasak saxena <158446050+Kasaksaxena@users.noreply.github.com>
2025-08-27 21:49:35 +0530

    38281

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import RestaurantInfo
+from datetime import datetime
 
 # Create your views here.
 def homepage(request):
@@ -8,4 +9,4 @@ def homepage(request):
         'restaurant_name':restaurant.name if restaurant else "Our Restaurant",
         'phone_number':settings.RESTAURANT_PHONE_NUMBER
     }
-    return render(request,'home/index.html',context)
+    return render(request,'home/index.html',context,{"current_year":datetime.now().year})
